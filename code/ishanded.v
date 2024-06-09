f3891df41a410701af4014bb9cdbf837aaa27065
minjoooo303 <156404156+minjoooo303@users.noreply.github.com>
2024-06-09 21:46:31 +0900

    Update ishanded.v

--- code/ishanded.v
+++ code/ishanded.v
@@ -1,9 +1,9 @@
 module p1handed(
-    input[3:0]state,
+    input[2:0]state,
     output p1handed
 );  
     wire p1handed, clk, d;
-    assign clk = ~state[2]&~state[1]&state[0] | ~state[2]&state[1]&state[0];
+    assign clk = ~state[2]&~state[1]&state[0] | ~state[2]&state[1]&state[0];//p1turn에서 reset하면 초기화 P1handedr가 1로 유지 될지도..
     assign d= p1handed & ~state[0] | p1handed & state[2] | state[1] & state[0];
 
     edge_trigger_DFF p1handed_(d,clk, 0, p1handed, ~p1handed);
@@ -12,7 +12,7 @@ module p1handed(
 endmodule
 
 module p2handed(
-    input[3:0]state,
+    input[2:0]state,
     output p2handed
 );
     wire p2handed;
