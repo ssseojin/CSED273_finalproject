13a54fabe296ded2f4a5d865546765984145f8d3
minjoooo303 <156404156+minjoooo303@users.noreply.github.com>
2024-06-09 18:46:47 +0900

    Create ishanded.v

---
+++ code/ishanded.v
@@ -0,0 +1,23 @@
+module p1handed(
+    input[3:0]state,
+    output p1handed
+);  
+    wire p1handed, clk, d;
+    assign clk = ~state[2]&~state[1]&state[0] | ~state[2]&state[1]&state[0];
+    assign d= p1handed & ~state[0] | p1handed & state[2] | state[1] & state[0];
+
+    edge_trigger_DFF p1handed_(d,clk, 0, p1handed, ~p1handed);
+
+
+endmodule
+
+module p2handed(
+    input[3:0]state,
+    output p2handed
+);
+    wire p2handed;
+    assign clk = ~state[2]&~state[1]&state[0] | state[2]&~state[1]&~state[0];
+    assign d= p2handed & state[1] | p2handed & ~state[0] | state[2] & ~state[1];
+    edge_trigger_DFF p1handed_(d,clk, 0, p2handed, ~p2handed);
+
+endmodule
